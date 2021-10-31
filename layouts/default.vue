cb50e3f56c50bf747dc176950c63494c3a6113f8
Ngoc Do <ngocdo@maruweb.vn>
2021-10-31 12:46:48 +0700

    Test deploy

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 
       <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold leading-tight text-gray-900">... xxx</h1>
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">How to change this title?</h1>
         </div>
       </header>
 
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="px-4 py-6 sm:px-0">
     <h1 v-if="error.statusCode === 404">{{ $t('page_not_found') }}</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">{{ $t('home_page') }}</NuxtLink>
